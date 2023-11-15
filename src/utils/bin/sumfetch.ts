363be775c3fad3b2018ff2263588132bdbabe44b
eduard-unplugged <eduard@unplugged.com>
2023-11-15 10:52:55 +0200

    adding fix

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -30,14 +30,44 @@ const sumfetch = async (args: string[]): Promise<string> => {
    ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓           <u><a href="${config.resume_url}" target="_blank">resume</a></u>
 ▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
 ▐▓                                 ▐▓       -----------
-▐▓        > L I V E T E R M        ▐▓        CONTACT 
+▐▓        > LINUX RULES            ▐▓        CONTACT 
 ▐▓                                 ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
 ▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
    ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓           <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓           -----------
-      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀             DONATE 
-        ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀                <u><a href="${config.donate_urls.paypal}" target="_blank">${config.donate_urls.paypal}</a></u>
-            ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                   <u><a href="${config.donate_urls.patreon}" target="_blank">${config.donate_urls.patreon}</a></u>
+      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀             EXPERIENCE
+        ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀                  DevOps engineer in Unplugged Systems Ltd., Yokneam-illit Israel (https://.unplugged.com)
+            ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                        December 20 -2021// current
+                                               System administration specialist  in Ravitzit,Amitnet -Haifa, Israel (https://amit-net.co.il)
+                                                  April '15 – 2018//December 20 - 2021 
+                                            -----------
+                                             EDUCATION AND SKILLS
+
+                                              2014 - 2016 Belarus National Technical College 
+                                              2018 - 2019  Tel-ran Education Center
+                                              2017-current Self Learning on Udemy, EdX, Linkedin course
+                                              2023- current BS Computer Science at University of People 
+            
+            
+                                              SKILLS
+                                              Programming languages:  Golang(Go), Java
+                                              Scripting languages: Bash, Python, Javascript
+                                              Databases:PostgreSQL, MySQL, Redis, MongoDB.
+                                              Containerization/Virtualisation:Docker,Kubernetes, VMware ESXI
+                                              Version control systems: Github, Gitlab, Jira , Maven 
+                                              CI/CD:Jenkins,GitActions, GitlabCI/CD  
+                                              VPN : WireGuard, OpenVPN, AWS VPN
+                                              Clouds: AWS, Google Cloud, Azure
+                                              Cloud automation tools: Helm,Terraform, Ansible
+                                              Security tools: Vault Hashicorp, Keycloak
+                                              Monitoring tools:  Grafana, Prometheus, Elastic Stack
+                                            -----------
+                                            爵 Languages
+                                              Hebrew,  
+                                              English, 
+                                              Russian
+            
+            
 
 `;
   }
